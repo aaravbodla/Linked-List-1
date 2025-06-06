e857141707922d81385d5b40c3ca7a40679bd2fe
Aarav Bodla <147215351+aaravbodla@users.noreply.github.com>
2025-06-06 22:41:37 +0530

    Create Linked_List_Cycle_ii.java

---
+++ Linked_List_Cycle_ii.java
@@ -0,0 +1,42 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : None
+// Your code here along with comments explaining your approach
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean flag = false;
+
+        while(fast.next != null && fast.next.next != null && !flag){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                flag = true;
+            }
+        }
+        if(!flag){
+            return null;
+        }
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
